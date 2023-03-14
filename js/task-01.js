acacc8d79b68445d2322ca3dcee5b3dabe9dbad5
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-03-14 09:46:56 +0200

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -22,8 +22,14 @@
 const items = document.querySelectorAll('.item');
 
 console.log(`Number of categories: ${items.length} `);
-Array.prototype.forEach.call(items, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.log(`Category: ${title} / Elements: ${itemsLength}`);
-});
+
+items.forEach(el => {
+    console.log("Category:", el.firstElementChild.textContent);
+    console.log("Elements:", el.lastElementChild.childElementCount)
+    })
+
+// Array.prototype.forEach.call(items, (element) => {
+//     const title = element.querySelector('h2').innerHTML;
+//     const itemsLength = element.querySelectorAll('li').length;
+//     console.log(`Category: ${title} / Elements: ${itemsLength}`);
+// });
